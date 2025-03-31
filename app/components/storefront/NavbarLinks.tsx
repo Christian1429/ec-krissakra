d81d57c6992b5b6c7e2271a951d101588001a121
Christian <christianlindberg01@hotmail.com>
2025-03-31 12:39:29 +0200

    product id n style

--- app/components/storefront/NavbarLinks.tsx
+++ app/components/storefront/NavbarLinks.tsx
@@ -1,4 +1,7 @@
+"use client"
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { cn } from '@/lib/utils';
 
 export const navbarLinks = [
   {
@@ -24,10 +27,11 @@ export const navbarLinks = [
 ];
 
 export function NavbarLinks() {
+  const location = usePathname();
   return (
-    <div className="hidden md:flex justify-center gap-x-4 items-center ml-8">
+    <div className="hidden md:flex justify-center gap-x-2 items-center ml-8">
       {navbarLinks.map((item) => (
-        <Link href={item.href} key={item.id} className="font-medium">
+        <Link href={item.href} key={item.id} className={cn(location === item.href ? 'bg-muted':"hover:bg-muted hover:bg-opacity-75", "group p-2 font-medium rounded-md")}>
           {item.name}
         </Link>
       ))}
